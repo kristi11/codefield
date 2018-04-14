3ee895d59a835b00293324e4526f7a322637f74f
kristi tanellari <tanellari@gmail.com>
2018-04-14 16:11:45 -0400

    extended user functionality

--- resources/views/client/clientGallery.blade.php
+++ resources/views/client/clientGallery.blade.php
@@ -64,8 +64,8 @@
 								@endif
 								<a type="button" class="btn btn-white btn-just-icon btn-xs btn-round hidden-xs" 
 								 href="{{ url('addToFavorties/'.$img->id) }}"
-								 rel="tooltip" data-placement="bottom" title="" data-original-title="{{$img->user->name}}">
-									Author: <i style="color: black" class="material-icons">person</i></a>
+								 rel="tooltip" data-placement="bottom" title="" data-original-title="Author: {{$img->user->name}}">
+									<i style="color: black" class="material-icons">person</i></a>
 								</a>
 								<a class=" hidden-sm hidden-md hidden-lg" href="{{ url('photos/'.$img->gallery_image) }}">
 								<i style="color: white" class="material-icons">more_horiz</i></a>		
