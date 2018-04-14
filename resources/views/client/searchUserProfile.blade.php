253d6a6dc1a7178d7321e48dbf2b546f12958c80
kristi tanellari <tanellari@gmail.com>
2018-04-14 18:11:27 -0400

    extended user functionality

--- resources/views/client/searchUserProfile.blade.php
+++ resources/views/client/searchUserProfile.blade.php
@@ -3,22 +3,24 @@
 <div class="col-xs-12">
 	<div class="card text-center">
 		<div class="card-content">
-			<h2>{{ $data->user->name }}</h2>
+			@foreach ($data as $d)
+			<h2>{{ $d->user->name }}</h2>
 			{{--<a href="{{ url(Auth::user()->slug.'/editProfile') }}">Edit</a>--}}			
  			{{-- <h5>{{ Auth::user()->website }}</h5> --}}
-			<h5>{{ $data->user->email }}</h5>
-			<a href="{{ url('/'.$data->user->slug.'/u/projects') }}" 
+			<h5>{{ $d->user->email }}</h5>
+			<a href="{{ url('/'.$d->user->slug.'/u/projects') }}" 
 				class="btn btn-simple btn-default">Projects
 				@if( $countUProjects != 0 ) 
 					<span class="label label-danger">{{$countUProjects}}</span>
 				@endif
 			</a>
-			<a href="{{ url('/'.$data->user->slug.'/u/photos') }}"
+			<a href="{{ url('/'.$d->user->slug.'/u/photos') }}"
 			 class="btn btn-simple btn-default">Photos
 			 @if( $countUPhotos != 0 ) 
 			 	<span class="label label-danger">{{$countUPhotos}}</span>
 			 @endif
 			</a>
+			@endforeach
 		</div>			
 			@yield('uProjects')
 			@yield('uPhotos')
