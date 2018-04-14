0cc8a3fc91f6c49c40b47948ab5f87ee64741555
kristi tanellari <tanellari@gmail.com>
2018-04-14 18:20:53 -0400

    extended user functionality

--- resources/views/client/searchUserProfile.blade.php
+++ resources/views/client/searchUserProfile.blade.php
@@ -3,10 +3,10 @@
 <div class="col-xs-12">
 	<div class="card text-center">
 		<div class="card-content">
-			<h2>{{ $user->name }}</h2>
+			<h2>{{ $user['name'] }}</h2>
 			{{--<a href="{{ url(Auth::user()->slug.'/editProfile') }}">Edit</a>--}}			
  			{{-- <h5>{{ Auth::user()->website }}</h5> --}}
-			<h5>{{ $user->email }}</h5>
+			<h5>{{ $user['email'] }}</h5>
 			<a href="{{ url('/'.$user->slug.'/u/projects') }}" 
 				class="btn btn-simple btn-default">Projects
 				@if( $countUProjects != 0 ) 
