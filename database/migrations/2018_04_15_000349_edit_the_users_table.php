f5288b4e89af8c97e3b0e950546089db51a5b532
kristi tanellari <tanellari@gmail.com>
2018-04-14 20:07:35 -0400

    extended user functionality

---
+++ database/migrations/2018_04_15_000349_edit_the_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EditTheUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned()->index()->change();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
