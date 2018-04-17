37fdd21ef87ddde327c7817333a7379951a18081
kristi tanellari <tanellari@gmail.com>
2018-04-17 04:53:53 -0400

    changes

--- resources/views/client/client_header.blade.php
+++ resources/views/client/client_header.blade.php
@@ -63,16 +63,10 @@
 {{--     <script src='https://www.google.com/recaptcha/api.js'></script>
  --}}
  <script>
-   if ('serviceWorker' in navigator && 'PushManager' in window) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
+   if ("serviceWorker" in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('./service-worker.js');
+  })
 }
  </script>
 </head>
