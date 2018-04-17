773307a2434ddb9854444537cacc91a75d80e8af
kristi tanellari <tanellari@gmail.com>
2018-04-17 04:38:22 -0400

    changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,42 +13,13 @@ self.addEventListener('install', function(event) {
   event.waitUntil(precache());
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
+var fromCache = function (request) {
+  return caches.open(cache_name).then(function (cache) {
+    cache.match(request).then(function (matching) {
+      return matching || Promise.resolve('no-match');
+    });
+  });
+}
+self.addEventListener('fetch', function (event) {
+  event.respondWith(fromCache(event.request));
 });
