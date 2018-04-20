294e7122c2bb3b7d4e50236766de7a1971813475
kristi tanellari <tanellari@gmail.com>
2018-04-20 02:12:48 -0400

    working on offline first

--- resources/views/client/client_header.blade.php
+++ resources/views/client/client_header.blade.php
@@ -85,4 +85,20 @@
     });
 }
  </script>
+ <script>
+   // Test via a getter in the options object to see if the passive property is accessed
+var supportsPassive = false;
+try {
+  var opts = Object.defineProperty({}, 'passive', {
+    get: function() {
+      supportsPassive = true;
+    }
+  });
+  window.addEventListener("testPassive", null, opts);
+  window.removeEventListener("testPassive", null, opts);
+} catch (e) {}
+
+// Use our detect's results. passive applied if supported, capture will be false either way.
+elem.addEventListener('touchstart', fn, supportsPassive ? { passive: true } : false); 
+ </script>
 </head>
