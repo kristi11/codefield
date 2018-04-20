4693d2f7c1bc330e40dc63310e26b5d3d61561d6
kristi tanellari <tanellari@gmail.com>
2018-04-20 02:25:49 -0400

    working on offline first

--- resources/views/client/client_header.blade.php
+++ resources/views/client/client_header.blade.php
@@ -68,5 +68,18 @@
             'csrfToken' => csrf_token(),
         ]) !!};
     </script>
-{{--<script src='https://www.google.com/recaptcha/api.js'></script>--}}
+{{--<script src='https://www.google.com/recaptcha/api.js'></script> --}}
+ <script>
+  if ('serviceWorker' in navigator ) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+ </script>
 </head>
