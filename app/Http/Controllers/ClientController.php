0e69d063127ba4498df2a4f71f03cd13a1146383
kristi tanellari <tanellari@gmail.com>
2018-04-21 05:30:26 -0400

    working on google fonts

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -240,7 +240,7 @@ class ClientController extends Controller
     {
     $title = 'Google Fonts';
     $result = Cache::remember(auth()->user()->id.'google_fonts', 60, function () use($title)  {
-    $url = "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDsCHWKh6jyNvXCJRFgDB2yio9lUCu9O0c";
+    $url = "https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyDsCHWKh6jyNvXCJRFgDB2yio9lUCu9O0c"->Paginate(10);
      return json_decode(file_get_contents( $url ));
      });
     // foreach ( $result->items as $font )
