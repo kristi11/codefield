c0f590a3643e8587550b06fc0a40a0b969cde8a8
kristi tanellari <tanellari@gmail.com>
2018-04-21 05:22:47 -0400

    working on google fonts

--- resources/views/client/fontLoadjs.blade.php
+++ resources/views/client/fontLoadjs.blade.php
@@ -1,5 +1,5 @@
-<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
-{{-- <script>
+{{-- <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+ --}}{{-- <script>
   WebFont.load({
     google: {
       families: [@foreach($result->items as $font)'{{$font->family}}',@endforeach]
