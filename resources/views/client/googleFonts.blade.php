252c6213ae2f69a316c495f78a727035d465f24b
kristi tanellari <tanellari@gmail.com>
2018-04-21 05:53:54 -0400

    working on google fonts

--- resources/views/client/googleFonts.blade.php
+++ resources/views/client/googleFonts.blade.php
@@ -53,7 +53,7 @@
 	</div>
 	{{-- </div> --}}
 </div>
-<div class="text-center">{{ $result->items->links('admin.mdl-bootstrap') }}</div>
+<div class="text-center">{{ $result->items->links('admin.mdl-bootstrap')->toArray() }}</div>
 
 @include('client.fontLoadjs')
 @endsection
