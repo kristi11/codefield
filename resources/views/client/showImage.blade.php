d67dab93a4e67f67a32ac074e324a771762c7f5c
kristi tanellari <tanellari@gmail.com>
2018-04-25 23:16:01 -0400

    showWimage file changes

--- resources/views/client/showImage.blade.php
+++ resources/views/client/showImage.blade.php
@@ -10,8 +10,8 @@
           <picture>
             <img style="height:100%;object-fit: cover;" class="img-responsive img-rounded" 
             src="{{asset('storage/galleries/'.$gallery->gallery_image)}}" 
-            srcset="{{asset('storage/gallery_thumbnails/'.$gallery->gallery_image.' 800w')}} " 
-            sizes="(max-width:800px) 800w"
+            srcset="{{asset('storage/gallery_thumbnails/'.$gallery->gallery_image.' 1000w')}} " 
+            sizes="(max-width:1000px) 1000w"
             alt="{{$gallery->alternative_text}}">
           </picture>
         </div>
