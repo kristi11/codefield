43c1c36abda144b2db8accb152f36e5c0e787658
kristi tanellari <tanellari@gmail.com>
2018-04-25 22:58:34 -0400

    showWimage file changes

--- resources/views/client/showImage.blade.php
+++ resources/views/client/showImage.blade.php
@@ -8,10 +8,14 @@
         
         <div class="card-header card-chart">
           <picture>
-            <img style=" height:50%;object-fit: cover;" class="img-responsive img-rounded" 
+            <img style="     image-rendering: -moz-crisp-edges;
+    image-rendering: -o-crisp-edges;
+    image-rendering: -webkit-optimize-contrast;
+    -ms-interpolation-mode: nearest-neighbor; 
+    height:100%;object-fit: cover;" class="img-responsive img-rounded" 
             src="{{asset('storage/galleries/'.$gallery->gallery_image)}}" 
-{{--             srcset="{{asset('storage/gallery_thumbnails/'.$gallery->gallery_image)}} 800w" 
-            sizes="(max-width:800px) 800w" --}}
+            srcset="{{asset('storage/gallery_thumbnails/'.$gallery->gallery_image)}} 800w" 
+            sizes="(max-width:800px) 800w"
             alt="{{$gallery->alternative_text}}">
           </picture>
         </div>
