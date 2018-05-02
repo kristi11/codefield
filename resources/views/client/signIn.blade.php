e962f0143ee77b1ec7ad662b8c19c3d83f05ddd3
kristi tanellari <tanellari@gmail.com>
2018-05-02 00:08:04 -0400

    changed signin page

--- resources/views/client/signIn.blade.php
+++ resources/views/client/signIn.blade.php
@@ -1,52 +1,186 @@
 @extends('client.client')
 @include('errors')
 @section('signIn')
-<div style="" class="container">
-	<div class="login-container">
-	@if($flash = session('message'))
+<style type="text/css">
+     .off-canvas-sidebar .nav {
+        background-color: white;
+        height: 100vh;
+        margin-top: 0px;
+    }
+</style>
+<nav class="navbar hidden-md hidden-lg">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">{{config('app.name')}}</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
+            <img alt="Codefield Brand" src="{{asset('/public/img/whitebg.jpg')}}">
+          <ul class="nav navbar-nav">
+        <h1 class="card-title text-center" style="font-family: 'Alfa Slab One', serif;">Services</h1>
+            <table class="table table-hover" >
+                <tbody>
+                    <tr>
+                        
+                        <td>
+                            <h3><i class="fa fa-user fa-5x"></i> Portfolio</h3>
+                            <h5>
+                                Your profile works as a portfolio showing you latest work
+                            </h5>
+                        </td>
+                    </tr>
+                    <tr>                        
+                        <td>
+                            <h3><i class="fa fa-file-code-o fa-5x"></i> Submit Projects</h3>
+                            <h5>
+                                Have your work seen across fellow developers and designers
+                            </h5>
+                        </td>
+                    </tr>
+                    <tr>                      
+                        <td>
+                            <h3><i class="fa fa-camera fa-5x"></i> Upload Photos</h3>
+                            <h5>
+                                Share your photography with others like you
+                            </h5>
+                        </td>
+                    </tr>
+                    <tr>                       
+                        <td>
+                            <h3><i class="fa fa-download fa-5x"></i> Unlimited downloads</h3>
+                            <h5>
+                                Download everything accross the platform completely free
+                            </h5>
+                        </td>
+                    </tr>
+                    <tr>                       
+                        <td>
+                            <h3><i class="fa fa-font fa-5x"></i> Google Fonts</h3>
+                            <h5>
+                                Take advantage of the latest Google Fonts.Small descriptive tutorial available
+                            </h5>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+<div class="container">
+    @if($flash = session('message'))
     <div class="text-center"> @include('flashMessages.flashMessage')</div>
   @endif
-  <div class="col-xs-12 col-sm-6 col-md-6 col-sm-offset-3">
-	<div class="card card-profile">
-    <div class="card-avatar">
-        <a href="#pablo">
-            <img class="img" src="storage/codefield_logo/signin_logo.png" alt="Codefield logo">
-        </a>
-    </div>
+  <div  style="margin-bottom: 100px;" class="col-xs-12 col-sm-6 col-md-6 hidden-sm hidden-xs">
+    <div  class="card card-profile">
     <div class="content">
-        <h4 class="card-title" style="font-family: 'Alfa Slab One', serif;font-size: 32px;">{{ config('app.name') }}</h4>
-        <p class="card-content category">
+        <h1 class="card-title" style="font-family: 'Alfa Slab One', serif;">What you get</h1>
+            <div class="card-content table-responsive text-left ht" style="">
+                <table class="table table-hover" >
+                    <tbody>
+                        <tr>
+                            <td><i class="fa fa-user fa-5x"></i></td>
+                            <td>
+                                <h3>Portfolio</h3>
+                                <h5>
+                                    Your profile works as a portfolio showing you latest work
+                                </h5>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-file-code-o fa-5x"></i></td>
+                            <td>
+                                <h3>Submit Projects</h3>
+                                <h5>
+                                    Have your work seen across fellow developers and designers
+                                </h5>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-camera fa-5x"></i></td>
+                            <td>
+                                <h3>Upload Photos</h3>
+                                <h5>
+                                    Share your photography with others like you
+                                </h5>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-download fa-5x"></i></td>
+                            <td>
+                                <h3>Unlimited downloads</h3>
+                                <h5>
+                                    Download everything accross the platform completely free
+                                </h5>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><i class="fa fa-font fa-5x"></i></td>
+                            <td>
+                                <h3>Google Fonts</h3>
+                                <h5>
+                                    Take advantage of the latest Google Fonts.Small descriptive tutorial available
+                                </h5>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+  <div style="margin-bottom: 100px;"  class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-lg-6 col-lg-offset-0 col-md-offset-0">
+    <div class="card card-profile">
+    <div class="content">
+        <h1 class="card-title" style="font-family: 'Alfa Slab One', serif;">{{ config('app.name') }}</h1>
+        <h5 class="card-content">
+            Codefield is a small new platform helping developers,designers and even photographers find tools that will make completing their next project a whole lot easier.
+
+            From photos to simple html templates to different 
+            more complex projects you'll find it all in here.
+        </h5>
+        <h5 class="card-content">
             Sign in to {{config("app.name")}} to explore what we have to offer.If you dont have an account, one will be created automatically based on your provider
-        </p>
-                                    		
-	<a href="auth/facebook" class="btn btn-flat btn-round btn-info">
-		<i class="fa fa-facebook" aria-hidden="true"></i> sign in with facebook</a>
-	<a href="auth/github" class="btn btn-flat btn-round btn-danger">
-		<i class="fa fa-github" aria-hidden="true"></i> sign in with github</a>
+        </h5>
+                <div class="card-content">                          
+    <a href="auth/facebook" class="btn  btn-block btn-info">
+        <i class="fa fa-facebook" aria-hidden="true"></i> sign in with facebook</a>
+    <a href="auth/github" class="btn  btn-block btn-danger">
+        <i class="fa fa-github" aria-hidden="true"></i> sign in with github</a>
+    </div>
         </div>
     </div>
 </div>
 </div>
-</div>
-<footer class="footer text-center md-space-up">
-    <div class="container-fluid">
-        <ul class="nav navbar-nav">
-            <li>
-                <a href="#">&copy; <script>document.write(new Date().getFullYear())</script></a>
-            </li>
-            <li>
-                <a href="{{ url('/') }}">Codefield</a>
-            </li>
-            <li>
-                <a href="{{ url('/privacyPolicy') }}" rel="noopener" target="_blank">Privacy</a>   
-            </li>
-            <li>
-                <a href="{{ url('/termsAndConditions') }}" rel="noopener" target="_blank">Terms</a>   
-            </li>
-            <li>
-                <a href="{{ url('/cookiePolicy') }}" rel="noopener" target="_blank">Cookies</a>   
-            </li>
-        </ul>
-    </div>
-</footer>
+    <nav class="scroll navbar-fixed-bottom">       
+        <a class="btn btn-simple btn-default" 
+            href="{{ url('/') }}">&copy; <script>document.write(new Date().getFullYear())</script> Codefield
+        </a>               
+        <a class="btn btn-simple btn-default" 
+        href="{{ url('/privacyPolicy') }}" 
+        rel="noopener" 
+        target="_blank">
+            Privacy
+        </a>
+        <a class="btn btn-simple btn-default" 
+        href="{{ url('/termsAndConditions') }}" 
+        rel="noopener" 
+        target="_blank">
+            Terms
+        </a> 
+        <a class="btn btn-simple btn-default" 
+        href="{{ url('/cookiePolicy') }}" 
+        rel="noopener" 
+        target="_blank">
+            Cookies
+        </a>        
+    </nav>
 @endsection
