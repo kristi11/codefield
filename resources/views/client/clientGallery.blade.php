fd674e96a7831d20b145036071007db3e87d1d61
Kristi Tanellari <tanellari@gmail.com>
2018-05-15 19:54:37 -0400

    change image source for different sizes

--- resources/views/client/clientGallery.blade.php
+++ resources/views/client/clientGallery.blade.php
@@ -35,16 +35,11 @@
 				@foreach ($gallery as $img)
 				
 					<span class="c" >
-						{{-- <a href=""> --}}
-						<picture>
-						<source 
-							media="max-width: 768px" 
-							srcset="{{asset('storage/gallery_thumbnails/'.$img->gallery_image.' 768w')}}">	
-						<img {{-- style="width: auto;max-width: 600px;" --}}
-							class="image size fit"
+						{{-- <a href=""> --}}						
+							<img style="width: auto;max-width: 600px;" class="image size fit"
 							src="{{asset('storage/gallery_thumbnails/'.$img->gallery_image)}}"
+							srcset="{{asset('storage/gallery_thumbnails/'.$img->gallery_image.' 600w')}}" 
 							alt="{{$img->alternative_text}}">
-						</picture>
 						{{-- </a> --}}
 						<div class="middle text-center">
 								<a type="button" class="btn btn-white btn-just-icon btn-xs btn-round hidden-xs" href="{{ url('photos/'.$img->gallery_image) }}"
