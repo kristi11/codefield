30dc5adc553e108974d2402c1faa83366382935d
Kristi Tanellari <tanellari@gmail.com>
2018-05-25 01:59:25 -0400

    testing

--- resources/views/client/signIn.blade.php
+++ resources/views/client/signIn.blade.php
@@ -294,7 +294,7 @@ body, h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, {
 <td>
     <div class="card nospace">
         <a href="{{ url('g_photo/'.$p->gallery_image) }}" rel="tooltip" data-placement="bottom" data-original-title="See more..">
-            <img style="max-height: 198px;" class="img-responsive img-raised fit" 
+            <img  class="img-responsive img-raised fit" 
             src="{{url('storage/gallery_thumbnails/'.$p->gallery_image)}}"
             alt="{{$p->alternative_text}}">
         </a>
