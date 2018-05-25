2c08687f023342a5af40db02d1b90e17d7579bda
Kristi Tanellari <tanellari@gmail.com>
2018-05-25 01:28:49 -0400

    testing

--- resources/views/guest/g_project.blade.php
+++ resources/views/guest/g_project.blade.php
@@ -38,7 +38,7 @@
            </p> --}}
         </div>
       <h4 class="title" id="title">{{ $widget->title }}</h4>
-      <p class="category">{!! ucwords(substr(Purify::clean($widget->body),0,-150)) !!} ...</p>
+      <p class="category">{!! ucwords(substr(Purify::clean($widget->body),0,-150)) !!} ...Sign in to read more</p>
       <div class="text-right">
       <a href="/#signIn" class="btn btn-{{$item->font_color}}" type="submit" id="p_edit">
         Sign in to download
