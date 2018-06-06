d5ab81bb9d98a6fee0509eafa31e4207a415132f
Kristi Tanellari <tanellari@gmail.com>
2018-06-05 21:11:11 -0400

    responsive images

--- resources/views/client/clientGallery.blade.php
+++ resources/views/client/clientGallery.blade.php
@@ -29,7 +29,7 @@
 	
 	<div class="card">
 		<div class="card-content">			
-			<div class="flexbin flexbin-margin" >
+			<div class="{{-- flexbin flexbin-margin --}}" >
 				@foreach ($gallery as $img)
 					<span class="c" >
 						{{-- <a href=""> --}}						
