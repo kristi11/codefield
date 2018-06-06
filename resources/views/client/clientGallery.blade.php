9f0741d59e66a3b32d5ada872bd8206574ad3242
Kristi Tanellari <tanellari@gmail.com>
2018-06-05 22:00:22 -0400

    responsive images

--- resources/views/client/clientGallery.blade.php
+++ resources/views/client/clientGallery.blade.php
@@ -34,6 +34,7 @@
 					<span class="c" >
 						{{-- <a href=""> --}}						
 						<img style="width: auto;max-width: 600px;" class="lazyload" 
+						src="{{asset('storage/tiny_photos/'.$img->gallery_image)}}" 
 						data-sizes="auto"
 						data-src="{{asset('storage/medium_photos/'.$img->gallery_image)}}"
 						data-srcset="{{asset('storage/medium_photos/'.$img->gallery_image.' 860w')}},
