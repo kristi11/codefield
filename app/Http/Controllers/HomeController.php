693f69268833584cc4b2e6c4c8594634e251e483
Kristi Tanellari <tanellari@gmail.com>
2018-06-07 20:55:17 -0400

    add compression for images

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -192,7 +192,7 @@ class HomeController extends Controller
          // }
          // else
          // return view('admin.trash',compact('deleted_admins','deleted_projects','deleted_images','title'));
-        dd(Storage::allFiles('storage/trash'));
+        dd(Storage::allFiles('storage/galleries'));
     }
 
     public function permanently_delete_admin($id){
