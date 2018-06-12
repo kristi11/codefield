571f9a2172de21dbfa560de79d5b61feb54de46f
Kristi Tanellari <tanellari@gmail.com>
2018-06-12 02:26:56 -0400

    testing

--- resources/views/client/signIn.blade.php
+++ resources/views/client/signIn.blade.php
@@ -269,8 +269,9 @@ body, h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, {
                 </div>
                 <div class="card-content">
                   <div class="preload">
-                  <h4 class="title" id="title">{{ str_limit($widget->title,30) }}</h4>
+                  
                 </div>
+                <h4 class="title" id="title">{{ str_limit($widget->title,30) }}</h4>
                   @if($widget->downloads == 0)
                     <h5 class="category">No downloads</h5>
                   @elseif($widget->downloads == 1)
