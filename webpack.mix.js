4040d1a24733c51cf9a0b26f0ef2eb5afd66699f
Kristi Tanellari <tanellari@gmail.com>
2018-06-12 03:25:23 -0400

    testing

--- webpack.mix.js
+++ webpack.mix.js
@@ -43,7 +43,8 @@ mix.webpackConfig({
     new SWPrecacheWebpackPlugin({
         cacheId: 'pwa',
         filename: 'service-worker.js',
-        staticFileGlobs: ['public/**/*'],
+        staticFileGlobs: ['public/**/*.{css,eot,svg,ttf,woff,woff2,js,html,jpg,jpeg,png}',
+                          'public/storage/**/*.{css,eot,svg,ttf,woff,woff2,js,html,jpg,jpeg,png}'],
         minify: true,
         stripPrefix: 'public/',
         handleFetch: true,
