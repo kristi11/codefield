7f862c66a3fede7492104796ebab1abe606ee274
Kristi Tanellari <tanellari@gmail.com>
2018-06-12 02:12:40 -0400

    testing

--- resources/views/client/signIn.blade.php
+++ resources/views/client/signIn.blade.php
@@ -319,7 +319,7 @@ body, h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, {
 </div>
 <script>
   $(window).load(function() {
-  $("div").removeClass("preload");
+  $("h4").removeClass("preload");
 });
 </script>
 @endsection
