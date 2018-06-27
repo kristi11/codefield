d798998c580afa08fa61276d48da1dbfd4b61145
Kristi Tanellari <tanellari@gmail.com>
2018-06-27 18:48:24 -0400

    extend user functionality

--- resources/views/client/partials/uProjects.blade.php
+++ resources/views/client/partials/uProjects.blade.php
@@ -3,39 +3,83 @@
 @if(count($data)<=0)
  <h3>{{$p}}</h3>
  @else
-<div class="col-md-8 col-md-offset-2">
-<div class="card-content table-responsive">
- <table class="table">
-	<thead class="text-default">
-		<tr>
-			<th style="border:none;" class="text-center">Product name</th>
-			<th style="border:none;" class="text-center">Link</th>
-			<th style="border:none;" class="text-center">Product status</th>
-		</tr>
-	</thead>
-	<tbody>
-		@foreach ($data as $d)
-		<tr>
-			<td style="border:none;">{{ $d->name }}</td>
-			<td style="border:none;"><a href="{{$d->github_repo }}" target="_blank" rel="noopener">Link</a></td>
-			@if ($d->status == 'approved')
-			<td style="border:none;">
-				<button class="btn btn-simple btn-success btn-sm">{{ $d->status }}</button>
-			</td>
-			@elseif ($d->status == 'pending')
-			<td style="border:none;">
-				<button class="btn btn-simple btn-warning btn-sm">{{ $d->status }}</button>
-			</td>
-			@else
-			<td style="border:none;">
-				<button class="btn btn-simple btn-danger btn-sm">{{ $d->status }}</button>
-			</td>
-			@endif
-		</tr>
-		@endforeach
-	</tbody>
-</table>
+@foreach ($data as $widget)
+<div class="col-lg-6 col-md-6 col-sm-6">
+  <div class="card fe_pr">
+    <div class="pr_img">
+      <div class="ct-chart" id="dailySalesChart">
+      <picture>
+       <img
+         style="max-height: 300px;"
+         class="lazyload blur-up img-responsive img-rounded fit"
+         src="{{asset('storage/avatars/'.'placeholder-'.$widget->image)}}"
+         data-src="{{asset('storage/avatars/'.$widget->image)}}"
+         alt="{{$widget->alternative_text}}">
+       </picture>
+      </div>
+    </div>
+    <div class="row">
+    <div class="text-center">
+      <a href="{{ url('products/'.$widget->title) }}" type="button" class="btn btn-danger btn-simple" rel="tooltip" data-placement="bottom" title="" data-original-title="Read more..." rel="noopener" >
+      <i class="material-icons">search</i>
+      </a>
+
+     @if($widget->zip_file)
+        <button hidden="">
+        <form action="{{ url('w/'.$widget->id) }}" method="POST" >
+        {{ csrf_field() }}
+        <button  type="submit" class="btn btn-primary btn-simple" rel="tooltip" data-placement="bottom" title="" data-original-title="Download" >
+        <i class="material-icons">file_download</i>
+        </button>
+        </form>
+        </button>
+      @endif
+      @if($widget->tutorial != null)
+        <a href="{{ $widget->tutorial }}" target="_blank" type="button" class="btn btn-info btn-simple"
+          rel="tooltip" data-placement="bottom" title="" data-original-title="See tutorial">
+          <i class="material-icons">videocam</i>
+        </a>
+      @endif
+      @if($widget->isFavorited())
+    <a type="button" class="btn btn-success btn-simple" href="{{ url('addToFavorties/'.$widget->id) }}"
+      rel="tooltip" data-placement="bottom" title="" data-original-title="In your favorites">
+      <i class="material-icons">favorite</i></a>
+    @else
+      <a type="button" class="btn btn-success btn-simple" href="{{ url('addToFavorties/'.$widget->id) }}"
+      rel="tooltip" data-placement="bottom" title="" data-original-title="Add to favorites">
+      <i class="material-icons">favorite_border</i></a>
+    @endif
+    </div>
+    </div>
+    <div class="card-content">
+      <h4 class="title" id="title">{{ str_limit($widget->dsc_title,30) }}</h4>
+      @if($widget->views == 0)
+        <h5 class="category">No views</h5>
+      @elseif($widget->views == 1)
+        <h5 class="category">{{$widget->views}} view</h5>
+      @else
+        <h5 class="category">{{$widget->views}} views</h5>
+      @endif
+
+      @if($widget->zip_file)
+        @if($widget->downloads == 0)
+          <h5 class="category">No downloads</h5>
+        @elseif($widget->downloads == 1)
+          <h5 class="category">{{$widget->downloads}} download</h5>
+        @else
+          <h5 class="category">{{$widget->downloads}} downloads</h5>
+        @endif
+        @else
+          <h5 class="category">Learning</h5>
+      @endif
+    </div>
+    <div class="card-footer">
+      <div class="stats">
+        <i class="material-icons">access_time</i>  Posted  {{ $widget->created_at->diffForHumans() }}
+      </div>
+    </div>
+  </div>
 </div>
-</div>
+@endforeach
 @endif
 @endsection
--- resources/views/client/partials/userProjects.blade.php
+++ resources/views/client/partials/userProjects.blade.php
@@ -3,39 +3,63 @@
 @if(count($data)<=0)
  <h3>{{$p}}</h3>
  @else
-<div class="col-md-8 col-md-offset-2">
-<div class="card-content table-responsive">
- <table class="table">
-	<thead class="text-default">
-		<tr>
-			<th style="border:none;" class="text-center">Product name</th>
-			<th style="border:none;" class="text-center">Link</th>
-			<th style="border:none;" class="text-center">Product status</th>
-		</tr>
-	</thead>
-	<tbody>
-		@foreach ($data as $d)
-		<tr>
-			<td style="border:none;">{{ $d->name }}</td>
-			<td style="border:none;"><a href="{{$d->github_repo }}" target="_blank" rel="noopener">Link</a></td>
-			@if ($d->status == 'approved')
-			<td style="border:none;">
-				<button class="btn btn-simple btn-success btn-sm">{{ $d->status }}</button>
-			</td>
-			@elseif ($d->status == 'pending')
-			<td style="border:none;">
-				<button class="btn btn-simple btn-warning btn-sm">{{ $d->status }}</button>
-			</td>
-			@else
-			<td style="border:none;">
-				<button class="btn btn-simple btn-danger btn-sm">{{ $d->status }}</button>
-			</td>
-			@endif
-		</tr>
-		@endforeach
-	</tbody>
-</table>
+@foreach ($data as $project)
+<div class="col-lg-6 col-md-6 col-sm-6">
+  <div class="card fe_pr">
+    <div class="pr_img">
+      <div class="ct-chart" id="dailySalesChart">
+      <picture>
+       <img style="max-height: 300px;" class="img-responsive img-rounded fit"
+       src="{{url('storage/avatars/'.$project->image)}}"
+       alt="{{$project->alternative_text}}">
+       </picture>
+      </div>
+    </div>
+    <div class="text-center">
+      <a href="{{ url('Uproducts/'.$project->id.'/edit') }}" type="button" class="btn btn-success btn-simple" rel="tooltip" data-placement="bottom" title="" data-original-title="Edit" rel="noopener" >
+      <i class="material-icons">edit</i>
+      </a>
+  	<button hidden="">
+      <form action="{{ url('deleteProduct/'.$project->id) }}" method="POST" >
+      {{method_field('DELETE')}}
+      {{ csrf_field() }}
+      <button  type="submit" class="btn btn-danger btn-simple" rel="tooltip" data-placement="bottom" title="" data-original-title="Remove" onclick="return confirm('Send {{$project->dsc_title}} to trash?');" >
+      <i class="material-icons">close</i>
+      </button>
+      </form>
+      </button>
+      @if($project->tutorial != null)
+        <a href="{{ $project->tutorial }}" target="_blank" type="button" class="btn btn-info btn-simple"
+          rel="tooltip" data-placement="bottom" title="" data-original-title="See tutorial">
+          <i class="material-icons">videocam</i>
+        </a>
+      @endif
+    </div>
+	<div class="card-content">
+      <h4 class="title" id="title">{{ str_limit($project->dsc_title,30) }}</h4>
+      @if($project->views == 0)
+        <p class="category">No views</p>
+      @elseif($project->views == 1)
+        <p class="category">{{$project->views}} view</p>
+      @else
+        <p class="category">{{$project->views}} views</p>
+      @endif
+
+      @if($project->downloads == 0)
+        <p class="category">No downloads</p>
+      @elseif($project->downloads == 1)
+        <p class="category">{{$project->downloads}} download</p>
+      @else
+        <p class="category">{{$project->downloads}} downloads</p>
+      @endif
+    </div>
+    <div class="card-footer">
+      <div class="stats">
+        <i class="material-icons">access_time</i>  Posted  {{ $project->created_at->diffForHumans() }}
+      </div>
+    </div>
+  </div>
 </div>
-</div>
+@endforeach
 @endif
 @endsection
