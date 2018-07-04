25c71726577fb9820b6178d164602c3db5aecfca
Kristi Tanellari <tanellari@gmail.com>
2018-07-04 17:24:10 -0400

    fix bugs

--- resources/views/guest/g_project.blade.php
+++ resources/views/guest/g_project.blade.php
@@ -54,10 +54,10 @@ a {
            </p> --}}
         </div>
       <h3 style="font-weight: bold;" class="title" id="title">{{ $widget->dsc_title }}</h3>
-      <p>{!! ucwords(substr(Purify::clean($widget->body),0,-150)) !!}...
-      </p>{{-- <button disabled="" class="btn btn-simple btn-xs"><a href="">...</a></button> --}}
+      <p>{!! ucwords(substr(Purify::clean($widget->body),0,-150)) !!}
+      </p>...{{-- <button disabled="" class="btn btn-simple btn-xs"><a href="">...</a></button> --}}
       <div class="text-right">
-      <a href="/#signIn" class="btn btn-{{$item->font_color}}" type="submit" id="p_edit">
+      <a href="/#signIn" class="btn btn-{{$item->font_color}} pull-right" type="submit" id="p_edit">
         Sign in to learn more
       </a>
       </div>
