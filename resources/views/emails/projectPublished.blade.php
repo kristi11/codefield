1f43c2d089938283a684789db8eca1f58c9e48f6
Kristi Tanellari <tanellari@gmail.com>
2018-07-17 17:32:09 -0400

    edit email content

--- resources/views/emails/projectPublished.blade.php
+++ resources/views/emails/projectPublished.blade.php
@@ -3,13 +3,13 @@
 
 <p>As requested we are letting you know that a new product was published at {{config('app.name')}}</p>
 
-@component('mail::button', ['url' => 'products/'.$project->title])
+@component('mail::button', ['url' => url('/products').'/'.$project->title])
 Check it out
 @endcomponent
 
 If you do not wish to recieve email notifications anymore you can click the button below and then click on account settings
 
-@component('mail::button', ['url' => '/'.$project->user->slug.'/products'])
+@component('mail::button', ['url' => url('/').'/'.$project->user->slug.'/products'])
 Manage email notifications
 @endcomponent
 
