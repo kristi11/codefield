8ddbde742bb6f87f9a242a4c8096271b914a6a9c
Kristi Tanellari <tanellari@gmail.com>
2018-10-28 22:12:28 -0400

    tiny mce improvements

--- resources/views/client/edit.blade.php
+++ resources/views/client/edit.blade.php
@@ -2,6 +2,7 @@
 
 @section('edit_userProject')
 @include('errors')
+<script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=gfnhsiax34xgkgxk89hb6mc24eo88l2utnhu4tvl2dc89kvg"></script>
 <script type="text/javascript">
   tinymce.init({
     selector: '#body',
