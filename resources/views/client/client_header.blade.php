c5f487dc70ecbf391f8250fb0261573d9dac7db6
Kristi Tanellari <tanellari@gmail.com>
2018-10-29 11:19:24 -0400

    change font

--- resources/views/client/client_header.blade.php
+++ resources/views/client/client_header.blade.php
@@ -25,7 +25,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
 {{--     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/0.11.1/trix.css">
  --}}
- <link href='//fonts.googleapis.com/css?family=Roboto:400,700,300|Baloo|Material+Icons' rel="stylesheet">
+ <link href='//fonts.googleapis.com/css?family=Baloo|Material+Icons' rel="stylesheet">
     <meta property="og:image" content="{{url('storage/codefield_logo/29695003_1752978298091934_4637625344562561024_n.png')}}" />
     <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-60.png">
     <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-76.png">
