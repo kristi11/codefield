eff2aaa708628429f67af260c1df74516c03b4c1
Kristi Tanellari <tanellari@gmail.com>
2018-12-04 23:28:59 -0500

    optimize js files

--- resources/views/client/client_footer.blade.php
+++ resources/views/client/client_footer.blade.php
@@ -1,5 +1,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" ></script>
-<script src="/js/app.js" async></script>
+<script src="/js/app.js"></script>
 {{-- <script src="/js/jquery-3.1.0.min.js" type="text/javascript"></script>
  --}}
 <script src="/js/bootstrap.min.js" type="text/javascript"></script>
