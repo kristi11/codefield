f0655ea6aca703d1398aced79bddac5a3818b913
Kristi Tanellari <tanellari@gmail.com>
2018-12-04 23:36:52 -0500

    optimize js files

--- resources/views/client/client_footer.blade.php
+++ resources/views/client/client_footer.blade.php
@@ -1,15 +1,14 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" async></script>
-<script src="/js/app.js" async></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="/js/app.js"></script>
 {{-- <script src="/js/jquery-3.1.0.min.js" type="text/javascript"></script>
  --}}
-<script src="/js/bootstrap.min.js" type="text/javascript" defer></script>
-<script src="/js/material.min.js" type="text/javascript" defer></script>
-<script src="/js/material-dashboard.min.js" type="text/javascript" defer></script>
-<script src="/js/bootstrap-filestyle.min.js" type="text/javascript" defer></script>
-<script src="/js/lazysizes.min.js" type="text/javascript" async></script>
+<script src="/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="/js/material.min.js" type="text/javascript"></script>
+<script src="/js/material-dashboard.min.js" type="text/javascript"></script>
+<script src="/js/bootstrap-filestyle.min.js" type="text/javascript"></script>
+<script src="/js/lazysizes.min.js" type="text/javascript"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-<script src="https://use.fontawesome.com/034c41ac18.js" async></script>
-{{-- dont forget to update disqus comments to the live site!!!! --}}
+<script src="https://use.fontawesome.com/034c41ac18.js"></script>
 <script id="dsq-count-scr" src="//codefield-1.disqus.com/count.js" async></script>
 <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
 <script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
